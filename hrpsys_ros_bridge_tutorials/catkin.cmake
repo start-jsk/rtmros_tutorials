19a44decf7d26328f99e18b99a8802ef86731c30
garaemon@gmail.com <garaemon@gmail.com@0e702958-57eb-198b-d1ae-3bee12c0c899>
2014-02-07 04:21:04 +0000

    fix dependency

--- hrpsys_ros_bridge_tutorials/catkin.cmake
+++ hrpsys_ros_bridge_tutorials/catkin.cmake
@@ -1,18 +1,19 @@
 cmake_minimum_required(VERSION 2.8.3)
 project(hrpsys_ros_bridge_tutorials)
 
-find_package(catkin REQUIRED COMPONENTS hrpsys_ros_bridge)
+#find_package(catkin REQUIRED COMPONENTS hrpsys_ros_bridge hrpsys openhrp3)
+find_package(catkin REQUIRED COMPONENTS hrpsys_ros_bridge) # pr2_controllers_msgs robot_monitor
+
+# find_package(PkgConfig)
+# pkg_check_modules(openhrp3 openhrp3 REQUIRED)
+# pkg_check_modules(hrpsys hrpsys-base REQUIRED)
+
 catkin_package(
     DEPENDS #
-    CATKIN-DEPENDS hrpsys_ros_bridge #
+    CATKIN-DEPENDS #
     INCLUDE_DIRS # TODO include
     LIBRARIES # TODO
 )
-
-catkin_package()
-
-pkg_check_modules(openhrp3 openhrp3 REQUIRED)
-pkg_check_modules(hrpsys hrpsys REQUIRED)
 
 if(EXISTS ${hrpsys_PREFIX}/share/hrpsys/samples/HRP4C/HRP4Cmain.wrl)
   compile_openhrp_model(
