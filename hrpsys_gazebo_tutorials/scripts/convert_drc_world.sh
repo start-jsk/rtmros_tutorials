246a03bc07105b0047b8d40fcd9a8700b2c09222
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2014-12-11 23:56:07 +0900

    add files for using drc_world with STARO

---
+++ hrpsys_gazebo_tutorials/scripts/convert_drc_world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+project_dir=$(rospack find hrpsys_gazebo_tutorials)
+
+input_file=$(rospack find drcsim_model_resources)/worlds/drc_practice_task_1.world
+output_file=${project_dir}/worlds/$(basename ${input_file} .world)_staro.world
+
+sed -e 's@<model_name>atlas@<model_name>STARO@g'  ${input_file} | \
+sed -e 's@<pin_link>utorso@<pin_link>BODY@g' > ${output_file}
