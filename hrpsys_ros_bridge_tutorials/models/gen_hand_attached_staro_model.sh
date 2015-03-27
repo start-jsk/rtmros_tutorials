4b489de90109d4229df99fb362b2dd46a9228bcb
YoheiKakiuchi <youhei@jsk.imi.i.u-tokyo.ac.jp>
2015-03-27 15:18:23 +0900

    update generation method for JAXON urdf

--- hrpsys_ros_bridge_tutorials/models/gen_hand_attached_staro_model.sh
+++ hrpsys_ros_bridge_tutorials/models/gen_hand_attached_staro_model.sh
@@ -12,6 +12,5 @@ BODY_FILE=${INPUT_FILE//.urdf/_body.urdf}
 
 tmp1=`mktemp`
 tmp2=`mktemp`
-${EUSCOLLADA_PATH}/scripts/remove_sensor_from_urdf.py HEAD_LINK1 $INPUT_FILE $tmp1
-${EUSCOLLADA_PATH}/scripts/remove_sensor_from_urdf.py LARM_LINK7 $tmp1 $tmp2
-${EUSCOLLADA_PATH}/scripts/remove_sensor_from_urdf.py RARM_LINK7 $tmp2 $BODY_FILE
+${EUSCOLLADA_PATH}/scripts/remove_sensor_from_urdf.py LARM_LINK7 $INPUT_FILE $tmp1
+${EUSCOLLADA_PATH}/scripts/remove_sensor_from_urdf.py RARM_LINK7 $tmp1 $BODY_FILE
