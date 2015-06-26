4651ca7194ecbf36791fd516436a3815f956cc1d
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2015-06-26 16:17:14 +0900

    update paramters for real robot

--- hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/urata_hrpsys_config.py
+++ hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/urata_hrpsys_config.py
@@ -33,9 +33,9 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         if self.ROBOT_NAME == "STARO":
             self.setStAbcParametersSTARO()
         elif self.ROBOT_NAME == "JAXON":
-            self.setStAbcParametersJAXON(foot="KAWADA")
+            self.setStAbcIcParametersJAXON(foot="KAWADA")
         elif self.ROBOT_NAME == "JAXON_RED":
-            self.setStAbcParametersJAXON_RED(foot="KAWADA")
+            self.setStAbcIcParametersJAXON(foot="KAWADA")
 
     def setStAbcParametersSTARO (self):
         # abc setting
@@ -123,71 +123,7 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         self.abc_svc.setGaitGeneratorParam(gg)
 
 
-    def setStAbcParametersJAXON(self, foot="KAWADA"):
-        # abc setting
-        abcp=self.abc_svc.getAutoBalancerParam()[1]
-        abcp.default_zmp_offsets=[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]];
-        abcp.move_base_gain=0.8
-        self.abc_svc.setAutoBalancerParam(abcp)
-        # kf setting
-        kfp=self.kf_svc.getKalmanFilterParam()[1]
-        kfp.R_angle=1000
-        self.kf_svc.setKalmanFilterParam(kfp)
-        # st setting
-        stp=self.st_svc.getParameter()
-        stp.st_algorithm=OpenHRP.StabilizerService.EEFM
-        stp.k_brot_p=[0, 0]
-        stp.k_brot_tc=[1000, 1000]
-        stp.eefm_body_attitude_control_gain=[0.5, 0.5]
-        stp.eefm_body_attitude_control_time_const=[1000, 1000]
-        stp.eefm_rot_damping_gain=20*1.6*1.1*1.5
-        stp.eefm_pos_damping_gain=[3500*1.6*3, 3500*1.6*3, 3500*1.6*1.1*1.5]
-        stp.eefm_rot_time_const=1.5/1.1
-        stp.eefm_pos_time_const_support=1.5/1.1
-        stp.eefm_wrench_alpha_blending=0.7
-        stp.eefm_pos_time_const_swing=0.06
-        stp.eefm_pos_transition_time=0.01
-        stp.eefm_pos_margin_time=0.02
-        # foot margin param
-        if foot == "KAWADA":
-            ## KAWADA foot : mechanical param is => inside 0.055, front 0.13, rear 0.1
-            stp.eefm_leg_inside_margin=0.05
-            stp.eefm_leg_front_margin=0.12
-            stp.eefm_leg_rear_margin=0.09
-        elif foot == "JSK":
-            ## JSK foot : mechanical param is -> inside 0.075, front 0.11, rear 0.11
-            stp.eefm_leg_inside_margin=0.07
-            stp.eefm_leg_front_margin=0.1
-            stp.eefm_leg_rear_margin=0.1
-        stp.eefm_zmp_delay_time_const=[0.055, 0.055]
-        stp.eefm_cogvel_cutoff_freq = 4.0
-        stp.eefm_k1=[-1.48412,-1.48412]
-        stp.eefm_k2=[-0.486727,-0.486727]
-        stp.eefm_k3=[-0.198033,-0.198033]
-        self.st_svc.setParameter(stp)
-        # Abc setting
-        #gg=self.abc_svc.getGaitGeneratorParam()[1]
-        #gg.stride_parameter=[0.1,0.05,10.0]
-        #gg.default_step_time=1.0
-        #self.abc_svc.setGaitGeneratorParam(gg)
-        gg=self.abc_svc.getGaitGeneratorParam()[1]
-        gg.default_step_time=1.2
-        #gg.default_double_support_ratio=0.32
-        gg.default_double_support_ratio=0.35
-        #gg.stride_parameter=[0.1,0.05,10.0]
-        #gg.default_step_time=1.0
-        #gg.swing_trajectory_delay_time_offset=0.35
-        gg.swing_trajectory_delay_time_offset=0.2
-        gg.stair_trajectory_way_point_offset=[0.03, 0.0, 0.0]
-        gg.swing_trajectory_final_distance_weight=3.0
-        gg.default_orbit_type = OpenHRP.AutoBalancerService.CYCLOIDDELAY
-        gg.toe_pos_offset_x = 1e-3*117.338;
-        gg.heel_pos_offset_x = 1e-3*-116.342;
-        gg.toe_zmp_offset_x = 1e-3*117.338;
-        gg.heel_zmp_offset_x = 1e-3*-116.342;
-        self.abc_svc.setGaitGeneratorParam(gg)
-
-    def setStAbcParametersJAXON_RED(self, foot="KAWADA"):
+    def setStAbcIcParametersJAXON(self, foot="KAWADA"):
         # abc setting
         abcp=self.abc_svc.getAutoBalancerParam()[1]
         #abcp.default_zmp_offsets=[[0.015, 0.0, 0.0], [0.015, 0.0, 0.0]];
@@ -204,19 +140,21 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         stp.st_algorithm=OpenHRP.StabilizerService.EEFMQP
         stp.k_brot_p=[0, 0]
         stp.k_brot_tc=[1000, 1000]
-        stp.eefm_body_attitude_control_gain=[0.5, 0.5]
         #stp.eefm_body_attitude_control_gain=[0, 0.5]
+        stp.eefm_body_attitude_control_gain=[0.5, 0.5]
         stp.eefm_body_attitude_control_time_const=[1000, 1000]
-        #stp.eefm_rot_damping_gain=20*1.6*1.1*1.5*1.4*1.3
-        stp.eefm_rot_damping_gain=20*1.6*1.1*1.5
-        stp.eefm_pos_damping_gain=[3500*1.6*3, 3500*1.6*3, 3500*1.6*1.1*1.5]
+        if self.ROBOT_NAME == "JAXON":
+            stp.eefm_rot_damping_gain=20*1.6*1.1*1.5*1.2
+            stp.eefm_pos_damping_gain=[3500*1.6*3, 3500*1.6*3, 3500*1.6*1.1*1.5*1.2]
+        elif self.ROBOT_NAME == "JAXON_RED":
+            stp.eefm_rot_damping_gain=20*1.6*1.1*1.5
+            stp.eefm_pos_damping_gain=[3500*1.6*3, 3500*1.6*3, 3500*1.6*1.1*1.5]
         stp.eefm_rot_time_const=1.5/1.1
         stp.eefm_pos_time_const_support=1.5/1.1
         stp.eefm_wrench_alpha_blending=0.7
         stp.eefm_pos_time_const_swing=0.06
         stp.eefm_pos_transition_time=0.01
         stp.eefm_pos_margin_time=0.02
-        # foot margin param
         # foot margin param
         if foot == "KAWADA":
             ## KAWADA foot : mechanical param is => inside 0.055, front 0.13, rear 0.1
@@ -255,8 +193,23 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         gg.heel_pos_offset_x = 1e-3*-116.342;
         gg.toe_zmp_offset_x = 1e-3*117.338;
         gg.heel_zmp_offset_x = 1e-3*-116.342;
-        gg.optional_go_pos_finalize_footstep_num = 1;
+        gg.optional_go_pos_finalize_footstep_num=1
         self.abc_svc.setGaitGeneratorParam(gg)
+        # Ic setting
+        limbs = ['rarm', 'larm']
+        for l in limbs:
+            icp = self.ic_svc.getImpedanceControllerParam(l)[1]
+            icp.M_p = 0
+            icp.D_p = 2000
+            icp.K_p = 4000
+            icp.moment_gain = [1.0, 1.0, 1.0]
+            icp.M_r = 0
+            icp.D_r = 150
+            icp.K_r = 200
+            icp.ik_optional_weight_vector = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+            if os.environ["ROBOT"] == "JAXON_RED":
+                icp.reference_gain = 0.05
+            self.ic_svc.setImpedanceControllerParam(l, icp)
 
     def jaxonResetPose (self):
         return [0.0,0.0,-0.349066,0.698132,-0.349066,0.0,0.0,0.0,-0.349066,0.698132,-0.349066,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.698132,-0.349066,-0.087266,-1.39626,0.0,0.0,-0.349066,0.0,0.698132,0.349066,0.087266,-1.39626,0.0,0.0,-0.349066]
