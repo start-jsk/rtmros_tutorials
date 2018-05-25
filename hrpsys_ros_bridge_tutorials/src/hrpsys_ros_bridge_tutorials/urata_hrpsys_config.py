e8188066f750997a253c0762148879c4f4abd77b
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2018-05-25 15:49:16 +0900

    add chidori autost param

--- hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/urata_hrpsys_config.py
+++ hrpsys_ros_bridge_tutorials/src/hrpsys_ros_bridge_tutorials/urata_hrpsys_config.py
@@ -271,8 +271,8 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         # AutoSt setting
         stp=self.abc_svc.getStabilizerParam()
         #stp.st_algorithm=OpenHRP.AutoBalancerService.EEFM
-        #stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQP
-        stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQPCOP
+        stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQP
+        # stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQPCOP
         stp.emergency_check_mode=OpenHRP.AutoBalancerService.CP # enable EmergencyStopper for JAXON @ 2015/11/19
         stp.cp_check_margin=[0.05, 0.045, 0, 0.095]
         stp.k_brot_p=[0, 0]
@@ -296,10 +296,20 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
                                          [3500*1.6*6, 3500*1.6*6, 3500*1.6*1.1*1.5],
                                          [3500*1.6*6*0.8, 3500*1.6*6*0.8, 3500*1.6*1.1*1.5*0.8],
                                          [3500*1.6*6*0.8, 3500*1.6*6*0.8, 3500*1.6*1.1*1.5*0.8]]
+            # stp.eefm_rot_damping_gain = [[1e5*20*1.6*1.1*1.5, 1e5*20*1.6*1.1*1.5, 1e5],
+            #                                               [1e5*20*1.6*1.1*1.5, 1e5*20*1.6*1.1*1.5, 1e5],
+            #                                               [1e5*20*1.6*1.1*1.5*1.2, 1e5*20*1.6*1.1*1.5*1.2, 1e5],
+            #                                               [1e5*20*1.6*1.1*1.5*1.2, 1e5*20*1.6*1.1*1.5*1.2, 1e5]]
+            # stp.eefm_pos_damping_gain = [[1e5*3500*1.6*6, 1e5*3500*1.6*6, 1e5*3500*1.6*1.1*1.5],
+            #                                               [1e5*3500*1.6*6, 1e5*3500*1.6*6, 1e5*3500*1.6*1.1*1.5],
+            #                                               [1e5*3500*1.6*6*0.8, 1e5*3500*1.6*6*0.8, 1e5*3500*1.6*1.1*1.5*0.8],
+            #                                               [1e5*3500*1.6*6*0.8, 1e5*3500*1.6*6*0.8, 1e5*3500*1.6*1.1*1.5*0.8]]
             stp.eefm_swing_pos_damping_gain = stp.eefm_pos_damping_gain[0]
             stp.eefm_swing_rot_damping_gain = stp.eefm_rot_damping_gain[0]
             stp.eefm_rot_compensation_limit = [math.radians(30), math.radians(30), math.radians(10), math.radians(10)]
             stp.eefm_pos_compensation_limit = [0.08, 0.08, 0.050, 0.050]
+        stp.eefm_zmp_delay_time_const=[0, 0]
+        stp.use_zmp_truncation=True
         stp.eefm_swing_damping_force_thre=[200]*3
         stp.eefm_swing_damping_moment_thre=[15]*3
         stp.eefm_use_swing_damping=True
@@ -619,6 +629,72 @@ class URATAHrpsysConfigurator(HrpsysConfigurator):
         stp.eefm_k2=[-0.368975,-0.368975]
         stp.eefm_k3=[-0.169915,-0.169915]
         self.st_svc.setParameter(stp)
+        # AutoST setting
+        stp=self.abc_svc.getStabilizerParam()
+        # stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQPCOP
+        stp.st_algorithm=OpenHRP.AutoBalancerService.EEFMQP
+        stp.k_brot_p=[0, 0]
+        stp.k_brot_tc=[1000, 1000]
+        stp.eefm_body_attitude_control_gain=[0.5, 0.5]
+        stp.eefm_body_attitude_control_time_const=[1000, 1000]
+        stp.eefm_rot_damping_gain=[[20*1.6*1.1*1.5, 20*1.6*1.1*1.5*1.5, 1e5]]*2
+        stp.eefm_pos_damping_gain=[[3500*1.6*3, 3500*1.6*3, 3500*1.6*1.1*1.5*0.5]]*2
+        stp.eefm_rot_time_const=[[1.5/1.1, 1.5/1.1, 1.5/1.1]]*2
+        stp.eefm_pos_time_const_support=[[1.5/1.1, 1.5/1.1, 1.5/1.1]]*2
+        stp.eefm_use_swing_damping=True
+        stp.eefm_swing_pos_damping_gain = stp.eefm_pos_damping_gain[0]
+        stp.eefm_swing_rot_damping_gain = stp.eefm_rot_damping_gain[0]
+        stp.eefm_rot_compensation_limit = [math.radians(30), math.radians(30)]
+        stp.eefm_pos_compensation_limit = [0.05, 0.05]
+        stp.eefm_ee_error_cutoff_freq=20.0
+        stp.eefm_swing_rot_spring_gain=[[1.0, 1.0, 1.0]]*2
+        stp.eefm_swing_pos_spring_gain=[[1.0, 1.0, 1.0]]*2
+        stp.eefm_wrench_alpha_blending=0.7
+        stp.eefm_pos_time_const_swing=0.06
+        stp.eefm_pos_transition_time=0.01
+        stp.eefm_pos_margin_time=0.02
+        stp.use_zmp_truncation=True
+        stp.eefm_zmp_delay_time_const=[0, 0]
+        # foot margin param
+        ## KAWADA foot
+        #   mechanical param is => inside 0.055, front 0.13, rear 0.1
+        # stp.eefm_leg_inside_margin=0.05
+        # #stp.eefm_leg_inside_margin=0.04
+        # stp.eefm_leg_front_margin=0.12
+        # stp.eefm_leg_rear_margin=0.09
+        tmp_leg_inside_margin=0.065
+        tmp_leg_outside_margin=0.065
+        tmp_leg_front_margin=0.13
+        tmp_leg_rear_margin=0.1
+        # JSK foot
+        # # mechanical param is -> inside 0.075, front 0.11, rear 0.11
+        # stp.eefm_leg_inside_margin=0.07
+        # stp.eefm_leg_front_margin=0.1
+        # stp.eefm_leg_rear_margin=0.1
+        stp.eefm_leg_inside_margin=tmp_leg_inside_margin
+        stp.eefm_leg_outside_margin=tmp_leg_outside_margin
+        stp.eefm_leg_front_margin=tmp_leg_front_margin
+        stp.eefm_leg_rear_margin=tmp_leg_rear_margin
+        rleg_vertices = [OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[tmp_leg_front_margin, tmp_leg_inside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[tmp_leg_front_margin, -1*tmp_leg_outside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[-1*tmp_leg_rear_margin, -1*tmp_leg_outside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[-1*tmp_leg_rear_margin, tmp_leg_inside_margin])]
+        lleg_vertices = [OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[tmp_leg_front_margin, tmp_leg_outside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[tmp_leg_front_margin, -1*tmp_leg_inside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[-1*tmp_leg_rear_margin, -1*tmp_leg_inside_margin]),
+                         OpenHRP.AutoBalancerService.TwoDimensionVertex(pos=[-1*tmp_leg_rear_margin, tmp_leg_outside_margin])]
+        stp.eefm_support_polygon_vertices_sequence = map (lambda x : OpenHRP.AutoBalancerService.SupportPolygonVertices(vertices=x), [rleg_vertices, lleg_vertices])
+        # stp.eefm_zmp_delay_time_const=[0.055, 0.055]
+        stp.eefm_cogvel_cutoff_freq = 4.0
+        stp.eefm_k1=[-1.48412,-1.48412]
+
+        # stp.eefm_zmp_delay_time_const=[0.055, 0.055]
+        stp.eefm_cogvel_cutoff_freq = 4.0
+        # calculated by calculate-eefm-st-state-feedback-default-gain-from-robot *chidori*
+        stp.eefm_k1=[-1.38444,-1.38444]
+        stp.eefm_k2=[-0.368975,-0.368975]
+        stp.eefm_k3=[-0.169915,-0.169915]
+        self.abc_svc.setStabilizerParam(stp)
         # Abc setting
         #gg=self.abc_svc.getGaitGeneratorParam()[1]
         #gg.stride_parameter=[0.1,0.05,10.0]
