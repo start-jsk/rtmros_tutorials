d6b1c9933b0acf4daa368718f3896a0bbeadebd4
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2018-10-01 10:43:18 +0900

    [hrpsys_gazebo_tutorials]legjoint6のgainの変更,ログの対象の変更,HRP2NOの反映

--- hrpsys_gazebo_tutorials/scripts/rtcd_start_command.sh
+++ hrpsys_gazebo_tutorials/scripts/rtcd_start_command.sh
@@ -9,4 +9,4 @@ FNAME=/tmp/rtcd_${USER}_`date +%Y-%m-%d_%H%M%S`.log
 TMP_RTC_LIST_SETTING=$(rosrun hrpsys_gazebo_tutorials hrp`expr $HRP2NO + 2000`_gazebo_setup.py --getRTCList | grep "example" | grep "config_file")
 
 hrpsys_gazebo_tutorials_PATH=$(rospack find hrpsys_gazebo_tutorials)
-rosrun hrpsys_gazebo_tutorials hrp`expr $HRP2NO + 2000`_gazebo_setup.py --init | tee /tmp/jsk_hrp2_setup_${USER}.log & bash -c "roslaunch hrpsys_gazebo_tutorials rtcd.launch ARGS:=\"-f ${hrpsys_gazebo_tutorials_PATH}/config/rtcdRobotModeHRP2Common.conf ${TMP_RTC_LIST_SETTING}\" 2>&1 | tee ${FNAME};"
+rosrun hrpsys_gazebo_tutorials hrp`expr $HRP2NO + 2000`_gazebo_setup.py --init 2>&1 | tee /tmp/jsk_hrp2_setup_${USER}.log & bash -c "roslaunch hrpsys_gazebo_tutorials rtcd.launch ARGS:=\"-f ${hrpsys_gazebo_tutorials_PATH}/config/rtcdRobotModeHRP2Common.conf ${TMP_RTC_LIST_SETTING}\" 2>&1 | tee ${FNAME};"
